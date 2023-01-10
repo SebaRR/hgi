b318880d593ac97818ecbf9f376c54d5f649572e
Seba Ramirez <seba.ra.ra@gmail.com>
2023-01-10 17:46:14 -0300

    added tables

--- hgi/hgi_users/admin.py
+++ hgi/hgi_users/admin.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
-from hgi_users.models import Client
-from hgi_users.models import User
+from hgi_users.models import City, UserToken, Region, Client, User
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Client)
+admin.site.register(Region)
+admin.site.register(City)
+admin.site.register(UserToken)
