7bb6196c0bff63c0002b0ce4aa0676d2c4317cfc
Seba Ramirez <seba.ra.ra@gmail.com>
2023-01-20 13:00:55 -0300

    commented db local

--- hgi/hgi/settings.py
+++ hgi/hgi/settings.py
@@ -92,23 +92,23 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     
-    #'default': {
-    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #    'NAME': 'railway', 
-    #    'USER': 'postgres',
-    #    'PASSWORD': 'fpmvBvSjVUuzRaMlwzWF',
-    #    'HOST': 'containers-us-west-134.railway.app', 
-    #    'PORT': '7391',
-    #}
-    
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'hgi_db', 
+        'NAME': 'railway', 
         'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'PASSWORD': 'fpmvBvSjVUuzRaMlwzWF',
+        'HOST': 'containers-us-west-134.railway.app', 
+        'PORT': '7391',
     }
+    
+    #'default': {
+    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #    'NAME': 'hgi_db', 
+    #    'USER': 'postgres',
+    #    'PASSWORD': 'admin',
+    #    'HOST': '127.0.0.1', 
+    #    'PORT': '5432',
+    #}
     
 }
 
