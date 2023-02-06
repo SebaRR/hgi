e66b6d2a25b060e38c73f2c7a109ff4b52784b32
Seba Ramirez <seba.ra.ra@gmail.com>
2023-02-06 00:13:33 -0300

    added recurso to admin view

--- hgi/hgi_ventas/admin.py
+++ hgi/hgi_ventas/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from hgi_ventas.models import Presupuesto, OrdenCompra, ProductoOC, TipoOC, UnidadProducto, Partida
+from hgi_ventas.models import Presupuesto, OrdenCompra, ProductoOC, TipoOC, UnidadProducto, Partida, Recurso
 
 admin.site.register(Presupuesto)
 admin.site.register(TipoOC)
@@ -7,3 +7,4 @@ admin.site.register(OrdenCompra)
 admin.site.register(UnidadProducto)
 admin.site.register(ProductoOC)
 admin.site.register(Partida)
+admin.site.register(Recurso)
