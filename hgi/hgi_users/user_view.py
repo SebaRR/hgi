157033bd8cab261de70d833810528c04133b2dd8
Seba Ramirez <seba.ra.ra@gmail.com>
2023-02-08 23:52:21 -0300

    added print

--- hgi/hgi_users/user_view.py
+++ hgi/hgi_users/user_view.py
@@ -31,7 +31,7 @@ class UserViewSet(viewsets.ModelViewSet):
 
     def retrieve(self, request, pk):
         self.queryset = User.objects.all()
-        print(request.session.keys())
+        print(request.session['_auth_user_id'])
         user = self.get_object()
         data_user = self.serializer_class(user).data
         if user.position is not None:
