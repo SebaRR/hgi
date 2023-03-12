4fbf9b7e39c8561ee8744b2db14c7188d5f2bfb4
Seba Ramirez <seba.ra.ra@gmail.com>
2023-03-11 21:25:36 -0300

    fix

--- hgi/hgi/utils.py
+++ hgi/hgi/utils.py
@@ -90,63 +90,42 @@ def create_contrato_user_permission(contrato_data):
     contrato = Contrato.objects.get(id=contrato_data['id'])
 
     if contrato.responsable.id not in permissions.keys():
-        print("NO -> responsable " + str(contrato.responsable.id))
         permissions[contrato.responsable.id] = [1,]
     else:
-        print("SI -> responsable " + str(contrato.responsable.id))
-        print(permissions[contrato.responsable.id])
         permissions[contrato.responsable.id].append(1)
 
     if contrato.administrador.id not in permissions.keys():
-        print("NO -> administrador " + str(contrato.administrador.id))
         permissions[contrato.administrador.id] = [3,]
     else:
-        print("SI -> administrador " + str(contrato.administrador.id))
         permissions[contrato.administrador.id].append(3)
-        print(permissions[contrato.administrador.id])
 
     if contrato.visitador.id not in permissions.keys():
-        print("NO -> visitador " + str(contrato.visitador.id))
         permissions[contrato.visitador.id] = [2,]
     else:
-        print("SI -> visitador " + str(contrato.visitador.id))
         permissions[contrato.visitador.id].append(2)
-        print(permissions[contrato.visitador.id])
     
     if contrato.of_tecnica.id not in permissions.keys():
-        print("NO -> of_tecnica " + str(contrato.of_tecnica.id))
         permissions[contrato.of_tecnica.id] = [6,]
     else:
-        print("SI -> of_tecnica " + str(contrato.of_tecnica.id))
         permissions[contrato.of_tecnica.id].append(6)
-        print(permissions[contrato.of_tecnica.id])
     
     if contrato.compras.id not in permissions.keys():
-        print("NO -> compras " + str(contrato.compras.id))
         permissions[contrato.compras.id] = [5,]
     else:
-        print("SI -> compras " + str(contrato.compras.id))
         permissions[contrato.compras.id].append(5)
-        print(permissions[contrato.compras.id])
 
     if contrato.administrativo.id not in permissions.keys():
-        print("NO -> administrativo " + str(contrato.administrativo.id))
         permissions[contrato.administrativo.id] = [7,]
     else:
-        print("SI -> administrativo " + str(contrato.administrativo.id))
         permissions[contrato.administrativo.id].append(7)
-        print(permissions[contrato.administrativo.id])
 
     if contrato.prevencionista.id not in permissions.keys():
-        print("NO -> prevencionista " + str(contrato.prevencionista.id))
         permissions[contrato.prevencionista.id] = [4,]
     else:
-        print("SI -> prevencionista " + str(contrato.prevencionista.id))
-        print(permissions[contrato.prevencionista.id])
         permissions[contrato.prevencionista.id].append(4)
      
     for key, permission in permissions.items():
         user = User.objects.get(id=key)
-        PermisoContratoUser.objects.create(user=user,contrato=contrato,permisos=permission)
+        PermisoContratoUser.objects.create(user=user,contrato=contrato.id,permisos=permission)
 
     return
